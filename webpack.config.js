1762571efc38a46533a2f9fa533aca2492e418db
zahirul <zahirulopel@gmail.com>
2023-01-26 21:58:42 +0600

    Add webpack css module

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,14 @@ module.exports = {
     entry: {
         index: './src/index.js',
     },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
     plugins: [
         new HtmlWebpackPlugin({
             title: 'To-do',
