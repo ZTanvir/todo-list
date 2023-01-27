60740a35a63b1f1dceaf4f42eb10226c6bc39e85
zahirul <zahirulopel@gmail.com>
2023-01-27 10:55:43 +0600

    Add font awesome to the project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import _ from 'lodash';
-import "./style.css"
-import 'normalize.css'
+import "./style.css";
+import 'normalize.css';
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
 
 function component() {
 }
