df947db5347a870625140948374595524798804f
zahirul <zahirulopel@gmail.com>
2023-02-01 23:45:26 +0600

    Create project based on input data

--- src/index.js
+++ src/index.js
@@ -7,7 +7,18 @@ import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/brands';
 
 
-function component() {
-}
+(function createProject() {
+    const addBtnEl = document.querySelector(".btn-add");
+    const projectFieldEl = document.querySelector("#input-project-name");
+    const projectListEl = document.querySelector(".project-list");
 
+    addBtnEl.addEventListener("click", () => {
+        let divEl = document.createElement("div");
+        divEl.textContent = projectFieldEl.value;
+        projectListEl.appendChild(divEl);
+        //Clear project name
+        projectFieldEl.value = '';
+    })
+
+})();
 // document.body.appendChild(component());
