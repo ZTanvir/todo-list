d7c5f9fbc31f863adb00dc41bd0f3b288abba019
zahirul <zahirulopel@gmail.com>
2023-02-08 00:30:23 +0600

    Show and hide project folder input field based on click and cancel

--- src/index.js
+++ src/index.js
@@ -6,19 +6,27 @@ import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/brands';
 
+const addBtnEl = document.querySelector(".btn-add");
+const cancelBtnEl = document.querySelector(".btn-cancel");
+const projectFieldEl = document.querySelector("#input-project-name");
+const projectListEl = document.querySelector(".project-list");
+const addProjectEl = document.querySelector(".add-project-text");
+const projectField = document.querySelector(".add-project-field");
 
-(function createProject() {
-    const addBtnEl = document.querySelector(".btn-add");
-    const projectFieldEl = document.querySelector("#input-project-name");
-    const projectListEl = document.querySelector(".project-list");
-
-    addBtnEl.addEventListener("click", () => {
-        let divEl = document.createElement("div");
-        divEl.textContent = projectFieldEl.value;
-        projectListEl.appendChild(divEl);
-        //Clear project name
-        projectFieldEl.value = '';
-    })
-
-})();
-// document.body.appendChild(component());
+// Show project input field when click on add project
+addProjectEl.addEventListener("click", () => {
+    projectField.classList.toggle("hidden");
+})
+// Add a project to the list when click on add btn
+addBtnEl.addEventListener("click", () => {
+    let divEl = document.createElement("div");
+    divEl.textContent = projectFieldEl.value;
+    projectListEl.appendChild(divEl);
+    //Clear project name
+    projectFieldEl.value = '';
+    projectField.classList.toggle("hidden");
+})
+// Hide a project input field when click on cancel btn
+cancelBtnEl.addEventListener("click", () => {
+    projectField.classList.toggle("hidden");
+})
