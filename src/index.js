2a2db5f85c357704a7380ee96fd48b82c6753be4
zahirul <zahirulopel@gmail.com>
2023-02-15 23:51:36 +0600

    Store project as object

--- src/index.js
+++ src/index.js
@@ -12,22 +12,41 @@ const projectFieldEl = document.querySelector("#input-project-name");
 const projectListEl = document.querySelector(".project-list");
 const addProjectEl = document.querySelector(".add-project-text");
 const projectField = document.querySelector(".add-project-field");
+// btn
+const addTaskBtn = document.querySelector(".add-task-btn");
+
+// All projects
+let allProject = {};
 
 // Show project input field when click on add project
 addProjectEl.addEventListener("click", () => {
     projectField.classList.toggle("hidden");
 })
+
 // Add a project to the list when click on add btn
 addBtnEl.addEventListener("click", () => {
     let divEl = document.createElement("div");
     divEl.textContent = projectFieldEl.value;
     divEl.dataset.project = projectFieldEl.value;
     projectListEl.appendChild(divEl);
+    console.log(divEl.dataset.project);
+    // Add folder to project
+    let projectName = divEl.dataset.project;
+    allProject[projectName] = [];
+    console.log(allProject);
     //Clear project name
     projectFieldEl.value = '';
     projectField.classList.toggle("hidden");
 })
+
 // Hide a project input field when click on cancel btn
 cancelBtnEl.addEventListener("click", () => {
     projectField.classList.toggle("hidden");
 })
+
+// Create todo class
+const todo = (taskName, date, priority) => {
+
+    return { taskName, date, priority };
+}
+
